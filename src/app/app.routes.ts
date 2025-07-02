9ef679571f55a67f130de2123bc9c48b8aefce9a
BrunoFujisaki <brunofujisaki2004@gmail.com>
2025-07-01 23:17:55 -0300

    Dashboard home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,16 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { TelaInicialComponent } from './components/dashboard/tela-inicial/tela-inicial.component';
 
 export const routes: Routes = [
     {
     path: 'dashboard',
     component: DashboardComponent,
+    children: [ 
+      { path: '', redirectTo: 'home', pathMatch: 'full' }, 
+      { path: 'home', component: TelaInicialComponent },
+      
+    ]
   },
   {
     path: '',
